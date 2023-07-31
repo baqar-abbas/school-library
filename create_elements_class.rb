36c054e712cd964620853c706331a89ef3eb7980
baqar-abbas <baqar007@hotmail.com>
2023-08-01 03:32:21 +0500

    refactored the code using DRY< KISS AND yAGNI approach and keeping SOLID SRP and using SOLID and Ruby best practices iunder consideration.

--- create_elements_class.rb
+++ create_elements_class.rb
@@ -1,31 +1,13 @@
+# create_elements_class.rb
 require './book'
 require './person'
 require './rental'
 require './student'
 require './teacher'
-require './list_elements_class'
 
-class App
-  def initialize
-    @books = []
-    @people = []
-    @rentals = []
-    @list_elements = ListElementsClass.new
-  end
+class CreateElementsClass
 
-  ## LIST THE BOOKS
-
-  def list_books
-    @list_elements.list_books(@books) # Call the method from ListElementsClass passing @books as an argument
-  end
-
-  ## LIST PEOPLE
-
-  def list_people
-    @list_elements.list_people(@people) # Call the method from ListElementsClass passing @people as an argument
-  end
-
-  ## CREATE A STUDENT
+    ## CREATE A STUDENT
   def create_student
     print 'Student name: '
     name = gets.chomp
@@ -35,13 +17,11 @@ class App
     permission = gets.chomp.downcase
     parent_permission = permission == 'y'
     student = Person.new(age, name: name, parent_permission: parent_permission)
-    @people.push(student)
     puts 'Student created successfully'
-    puts ''
+    student
   end
 
   ## CREATE A TEACHER
-
   def create_teacher
     print 'Specialization: '
     specialization = gets.chomp
@@ -49,9 +29,9 @@ class App
     name = gets.chomp
     print 'Teacher age: '
     age = gets.chomp
-    @people << Teacher.new(age, specialization, name: name)
+    teacher = Teacher.new(age, specialization, name: name)
     puts 'Teacher created successfully'
-    puts ''
+    teacher
   end
 
   # Create a person
@@ -67,43 +47,33 @@ class App
     end
   end
 
-  ## CREATE A BOOK
-
+  # Create a book
   def create_book
     print 'Book title: '
     title = gets.chomp
     print 'Book author: '
     author = gets.chomp
     book = Book.new(title, author)
-    @books.push(book)
     puts 'Book created successfully'
-    puts ''
+    book
   end
-
-  ## CREATE A RENTAL
-
-  def create_rental
+  
+    # Create a rental
+  def create_rental(books, people)
     puts 'Select a book from the following list by number'
-    @books.each_with_index do |book, index|
+    books.each_with_index do |book, index|
       puts "#{index}) Title: #{book.title}, Author: #{book.author}"
     end
     book_index = gets.chomp.to_i
     puts 'Select a person from the following list by number (not id)'
-    @people.each_with_index do |person, index|
+    people.each_with_index do |person, index|
       puts "#{index}) Name: #{person.name}, Age: #{person.age}", "id: #{person.id}"
     end
     person_index = gets.chomp.to_i
     print 'Date: '
     date = gets.chomp
-    @rentals << Rental.new(@books[book_index], @people[person_index], date)
+    rental = Rental.new(books[book_index], people[person_index], date)
     puts 'Rental created successfully'
-    puts ''
+    rental
   end
-
-  ## LIST RENTALS BY PERSON
-
-def list_rentals_by_person_id
-    @list_elements.list_rentals_by_person_id(@people, @rentals)
-  end
-
 end
---
+++ list_elements_class.rb
@@ -0,0 +1,29 @@
+require './rental'
+
+class ListElementsClass
+    def list_books(books)
+      books.each do |book|
+        puts "Title: #{book.title}", "Author: #{book.author}"
+      end
+      puts ''
+    end
+  
+    def list_people(people)
+      people.each do |person|
+        puts "Name: #{person.name}", "age: #{person.age}", "id: #{person.id}"
+      end
+      puts ''
+    end
+  
+    def list_rentals_by_person_id(people, rentals)    
+      puts 'write person id: '
+      person_id = gets.chomp.to_i
+      person = people.find { |prson| prson.id == person_id }
+      rentals = rentals.select { |rental| rental.person == person }
+      rentals.each do |rental|
+        puts "Book: #{rental.book.title}, Book: #{rental.book.author} Date: #{rental.date}"
+      end
+      puts ''
+    end
+  end
+  
